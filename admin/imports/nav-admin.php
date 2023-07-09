04bd06f1c0ffe675b1a1d9c8bce5bd002f7fba64
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2023-07-09 13:36:05 +0530

    NavBar changes

--- admin/imports/nav-admin.php
+++ admin/imports/nav-admin.php
@@ -164,7 +164,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="index.php">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
@@ -174,7 +174,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="index.php">
                 <i class="bi bi-gear"></i>
                 <span>Account Settings</span>
               </a>
